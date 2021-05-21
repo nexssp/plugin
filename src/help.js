5c0da295e0aa3d1532dc537796c85472a428d034
Marcin Polak <mapoart@gmail.com>
2021-05-21 17:54:43 +0200

    init

--- src/help.js
+++ src/help.js
@@ -1,56 +1,53 @@
-require("@nexssp/extend")("array", "object"); // array flat / nodejs 10 + invert for object
+require('@nexssp/extend')('array', 'object') // array flat / nodejs 10 + invert for object
 // const aliases = require("../aliases.json").invert();
-const os = require("os");
-const { bold, grey } = require("@nexssp/ansi");
+const os = require('os')
+const { bold, grey } = require('@nexssp/ansi')
 // console.log(path.dirname(path.dirname(process.execPath)));
 
 exports.help = async (entries, plugin, path) => {
-  const aliases = require(`${path}/aliases.json`);
-  commandAliases = aliases.invert();
+  const aliases = require(`${path}/aliases.json`)
+  commandAliases = aliases.invert()
 
   // const EOL = require("os").EOL;
-  const EOL = "\n";
+  const EOL = '\n'
   const commandsHelp = await Promise.all(
     entries.map(async (entry) => {
-      const helpContent = require("fs")
-        .readFileSync(entry)
-        .toString()
-        .split(EOL);
+      const helpContent = require('fs').readFileSync(entry).toString().split(EOL)
       //   console.info(helpContent.toString());
-      const command = entry.match(/commands\/(.*).md$/)[1];
+      const command = entry.match(/commands\/(.*).md$/)[1]
       // .slice(-1)
       // .pop()
       // .replace(".md");
       //   console.log("!!!!!!", command[1]);
       //   process.exit(1);
       // We display 3rd or 1st line from help. Eg 1st can be just header as proper md file
-      const cmd = command.replace(/\.js/, "");
-      let cmdDisplay = cmd;
+      const cmd = command.replace(/\.js/, '')
+      let cmdDisplay = cmd
       if (commandAliases[cmd]) {
-        cmdDisplay = `${cmd}|${commandAliases[cmd]}`;
+        cmdDisplay = `${cmd}|${commandAliases[cmd]}`
       }
 
-      let pluginDisplay = plugin;
+      let pluginDisplay = plugin
       if (aliases[plugin]) {
-        pluginDisplay = `${plugin}|${aliases[plugin]}`;
+        pluginDisplay = `${plugin}|${aliases[plugin]}`
       }
 
-      if (cmdDisplay === ".gitkeep") {
-        cmdDisplay = "";
+      if (cmdDisplay === '.gitkeep') {
+        cmdDisplay = ''
       }
 
       const commandHelp =
         cmd !== plugin
           ? `${pluginDisplay} ${cmdDisplay}` // [args]
-          : `${pluginDisplay}`;
+          : `${pluginDisplay}`
 
       return {
         command: commandHelp,
         commandDesc: helpContent[2] || helpContent[0],
-      };
+      }
     })
-  );
-  if (global["NEXSSP_VERSION"])
+  )
+  if (global['NEXSSP_VERSION'])
     console.log(
       `                ____                                                              
 |..          | |             \`\`..      ..''             ..''''             ..'''' 
@@ -60,16 +57,14 @@ exports.help = async (entries, plugin, path) => {
 Programmer ${bold(NEXSSP_VERSION)}, NodeJS ${process.version}, OS: ${
         process.platform
       } ${os.release()}  `
-    );
+    )
 
   commandsHelp.flat().forEach((e) => {
-    console.log(grey("nexss"), bold(e.command), e.commandDesc);
-  });
+    console.log(grey('nexss'), bold(e.command), e.commandDesc)
+  })
   // console.log(commandsHelp.flat());
-  console.log(
-    bold("To display help add 'help': nexss command help OR nexss package help")
-  );
+  console.log(bold("To display help add 'help': nexss command help OR nexss package help"))
   //   console.log(JSON.stringify(result));
-};
+}
 
 // await fs.promises.mkdir("/tmp/a/apple", { recursive: true });
--- src/plugin.js
+++ src/plugin.js
@@ -1,8 +1,14 @@
 /**
+ * Copyright © 2018-2021 Nexss.com / Marcin Polak mapoart@gmail.com. All rights reserved.
+ * This source code is governed by MIT license, please check LICENSE file.
+ */
+
+/**
  * Creates Plugin definition (all params optional)
  * @param {string} plugin - path to the plugin
  * //@param {string} path - path to the plugin
  * @param {regexp|string} trigger - plugin cmd needs to pass the test
+ * @param {string} triggerValue - default is provess.argv[2]
  * @param {string} commandsPath - folder where the commands are
  * @param {string} benchmark - display executed time if enabled
  * @param {array} ommit - ommits files eg. '!*\/**\/*.nexss-test.js'
@@ -15,45 +21,43 @@ function nexssPlugin({
   trigger,
   triggerValue /* default is provess.argv[2] */,
   path,
-  commandsPath = "src/cli/commands",
+  commandsPath = 'src/cli/commands',
   through,
   benchmark = true,
   ommit = [],
 }) {
-  const { bold, green, red } = require("@nexssp/ansi");
-  const _path = require("path");
+  const { bold, green, red } = require('@nexssp/ansi')
+  const _path = require('path')
   if (!plugin && !path) {
-    throw new Error(red("'plugin' or 'path' is required for nexssPlugin."));
+    throw new Error(red("'plugin' or 'path' is required for nexssPlugin."))
   } else if (plugin && path) {
-    console.error(`Please us only one of the properties: plugin or path.`);
-    console.error(`You have used:`);
-    console.error(`plugin: ${plugin}`);
-    console.error(`path: ${path}`);
-    process.exit();
+    console.error(`Please us only one of the properties: plugin or path.`)
+    console.error(`You have used:`)
+    console.error(`plugin: ${plugin}`)
+    console.error(`path: ${path}`)
+    process.exit()
   }
 
-  let _ignore = ["!*/**/*.nexss-test.js"];
+  let _ignore = ['!*/**/*.nexss-test.js']
   if (ommit) {
-    _ignore = [..._ignore, ...ommit];
+    _ignore = [..._ignore, ...ommit]
   }
-  const _arguments = arguments;
-  const _log = require("@nexssp/logdebug");
-  const _name = plugin || "";
-  const __name = _name.split("/").slice(-1)[0];
+  const _arguments = arguments
+  const _log = require('@nexssp/logdebug')
+  const _name = plugin || ''
+  const __name = _name.split('/').slice(-1)[0]
 
-  if (_name) path = getPluginPath(plugin);
+  if (_name) path = getPluginPath(plugin)
 
-  _NEXSS_COMMANDS_FOLDER = _path.join(path, commandsPath);
+  _NEXSS_COMMANDS_FOLDER = _path.join(path, commandsPath)
 
   // If dynamic runCommand was executed recursive so already passed.
   if (aliases || trigger) {
-    trigger = aliases
-      ? new RegExp(`(${__name}|${aliases.join("|")})`)
-      : trigger;
-    _log.di(`@plugin: Trigger '${trigger}' exists - check..`);
+    trigger = aliases ? new RegExp(`(${__name}|${aliases.join('|')})`) : trigger
+    _log.di(`@plugin: Trigger '${trigger}' exists - check..`)
     if (!triggerValue) {
-      _log.di(`@plugin: TriggerValue does not exist. Using process.argv[2]`);
-      triggerValue = process.argv[2];
+      _log.di(`@plugin: TriggerValue does not exist. Using process.argv[2]`)
+      triggerValue = process.argv[2]
     }
 
     // const emptyPluginObject = {
@@ -64,78 +68,65 @@ function nexssPlugin({
 
     if (trigger instanceof RegExp) {
       if (!trigger.test(triggerValue)) {
-        _log.dy(
-          `@plugin: REGEXP: Trigger ${triggerValue} didn't pass the test: ${trigger}`
-        );
-        return false;
+        _log.dy(`@plugin: REGEXP: Trigger ${triggerValue} didn't pass the test: ${trigger}`)
+        return false
       } else {
-        _log.dg(
-          `@plugin: REGEXP: Trigger ${triggerValue} passed the test: ${trigger}`
-        );
+        _log.dg(`@plugin: REGEXP: Trigger ${triggerValue} passed the test: ${trigger}`)
       }
     } else if (triggerValue !== trigger) {
-      _log.d(
-        `@plugin: STRING: Trigger ${triggerValue} didn't pass the test: ${trigger}`
-      );
-      return false;
+      _log.d(`@plugin: STRING: Trigger ${triggerValue} didn't pass the test: ${trigger}`)
+      return false
     } else {
-      _log.dg(
-        `@plugin: STRING: Trigger ${triggerValue} passed the test: ${trigger}`
-      );
+      _log.dg(`@plugin: STRING: Trigger ${triggerValue} passed the test: ${trigger}`)
     }
   }
-  let _fs;
+  let _fs
 
-  let _version;
-  let _benchmark = benchmark;
+  let _version
+  let _benchmark = benchmark
 
   const start = () => {
-    _log.dg(`@plugin: starting ${_name} at: `, commandsPath);
-    _fs = require("fs");
+    _log.dg(`@plugin: starting ${_name} at: `, commandsPath)
+    _fs = require('fs')
 
-    const _packageJsonPath = _path.join(path, "package.json");
+    const _packageJsonPath = _path.join(path, 'package.json')
     if (_fs.existsSync(_packageJsonPath)) {
-      const { version, name } = require(_packageJsonPath);
-      _version = version;
+      const { version, name } = require(_packageJsonPath)
+      _version = version
       if (_name && _name !== name) {
         throw new Error(
-          red(
-            `Name of the plugin specified '${_name}' has not been matched with the '${name}'`
-          )
-        );
+          red(`Name of the plugin specified '${_name}' has not been matched with the '${name}'`)
+        )
       }
     }
 
     if (_name && !through) {
-      console.log(`${_name}@${bold(green(_version))}`);
-      if (_benchmark) console.time(bold(_name));
+      console.log(`${_name}@${bold(green(_version))}`)
+      if (_benchmark) console.time(bold(_name))
     }
-    _started = true;
-  };
+    _started = true
+  }
 
   function getHelpFiles() {
-    const fg = require("fast-glob");
-    const files = fg.sync(
-      [`${_NEXSS_COMMANDS_FOLDER}/*.md`.replace(/\\/g, "/")],
-      {
-        ignore: _ignore,
-      }
-    );
+    const fg = require('fast-glob')
+    const files = fg.sync([`${_NEXSS_COMMANDS_FOLDER}/*.md`.replace(/\\/g, '/')], {
+      ignore: _ignore,
+    })
 
-    return files;
+    return files
   }
 
   const getAliases = () => {
     if (_fs.existsSync(`${path}/aliases.json`)) {
-      return require(`${path}/aliases.json`);
+      return require(`${path}/aliases.json`)
     }
-  };
+  }
 
   function displayCommandHelp() {
-    const { help } = require("./help");
-    const helpFiles = getHelpFiles();
+    const { help } = require('./help')
+    const helpFiles = getHelpFiles()
     // console.log(helpFiles, __name, path);
-    help(helpFiles, __name, path);
+    help(helpFiles, __name, path)
     // process.exit(1);
   }
 
@@ -150,74 +141,65 @@ function nexssPlugin({
       args,
       dynamic,
       localArgs,
-    });
+    })
 
-    const commandAliases = getAliases();
+    const commandAliases = getAliases()
     if (commandAliases && commandAliases[command]) {
-      command = commandAliases[command];
+      command = commandAliases[command]
     } else {
-      _log.dg(`@plugin: aliases not found. `);
+      _log.dg(`@plugin: aliases not found. `)
     }
 
-    _log.dm();
+    _log.dm()
     // return false if not exists and go through is enabled (we just go through)
     if (!command && through) {
-      return false;
+      return false
     }
 
-    if (!command || command === "help") {
-      displayCommandHelp();
-      return;
+    if (!command || command === 'help') {
+      displayCommandHelp()
+      return
     }
 
-    const subpluginPath = _path.resolve(_NEXSS_COMMANDS_FOLDER, "../");
+    const subpluginPath = _path.resolve(_NEXSS_COMMANDS_FOLDER, '../')
 
-    const router = _path.join(subpluginPath, "_router.js");
-    _log.dy(`@plugin: checking router at:`, router);
-    subpluginPath;
+    const router = _path.join(subpluginPath, '_router.js')
+    _log.dy(`@plugin: checking router at:`, router)
+    subpluginPath
 
     if (_fs.existsSync(router)) {
-      _log.dg(`@plugin: router has been found:`, router);
-      const resultFromRouter = require(router)(
-        args[0],
-        args.slice(1),
-        command,
-        localArgs
-      );
-      _log.dg(`@plugin: router returned`, resultFromRouter);
+      _log.dg(`@plugin: router has been found:`, router)
+      const resultFromRouter = require(router)(args[0], args.slice(1), command, localArgs)
+      _log.dg(`@plugin: router returned`, resultFromRouter)
       // console.log({ resultFromRouter });
-      return resultFromRouter;
+      return resultFromRouter
     } else {
-      _log.dy(`@plugin: router has NOT been found:`, router);
-      const commandFile = `${_path.join(_NEXSS_COMMANDS_FOLDER, command)}.js`;
+      _log.dy(`@plugin: router has NOT been found:`, router)
+      const commandFile = `${_path.join(_NEXSS_COMMANDS_FOLDER, command)}.js`
       if (_fs.existsSync(commandFile)) {
-        if (args[0] !== "help") {
-          _log.dy(`@plugin: found command at:`, commandFile, `loading..`);
-          return require(commandFile)(command, args);
+        if (args[0] !== 'help') {
+          _log.dy(`@plugin: found command at:`, commandFile, `loading..`)
+          return require(commandFile)(command, args)
         } else {
-          _log.dg(`@plugin: loading help..`);
-          let mdExploded = commandFile.split(/\./);
-          mdExploded.pop();
-          const helpFile = `${mdExploded.join(".")}.md`;
-          const content = _fs.readFileSync(helpFile).toString();
-          const { displayMarkdown } = require("../lib/markdown");
-          displayMarkdown(content);
-          return true;
+          _log.dg(`@plugin: loading help..`)
+          let mdExploded = commandFile.split(/\./)
+          mdExploded.pop()
+          const helpFile = `${mdExploded.join('.')}.md`
+          const content = _fs.readFileSync(helpFile).toString()
+          const { displayMarkdown } = require('../lib/markdown')
+          displayMarkdown(content)
+          return true
         }
       } else {
-        _log.dr(
-          `@plugin: command NOT found at:`,
-          commandFile,
-          "Loading help for this command"
-        );
-        _log.dy(`Command '${command}' has not been found for ${_name}.`);
-        displayCommandHelp();
-        return true;
+        _log.dr(`@plugin: command NOT found at:`, commandFile, 'Loading help for this command')
+        _log.dy(`Command '${command}' has not been found for ${_name}.`)
+        displayCommandHelp()
+        return true
       }
     }
 
     if (_name && !through) {
-      if (_benchmark) console.timeEnd(bold(_name));
+      if (_benchmark) console.timeEnd(bold(_name))
     }
   }
 
@@ -227,18 +209,16 @@ function nexssPlugin({
     start,
     runCommand,
     displayCommandHelp,
-  };
+  }
 }
 
 function getPluginPath(plugin) {
-  const { dirname } = require("path");
-  const Module = module.constructor;
-  const moduleMainFilePath = dirname(
-    Module._resolveFilename(plugin, module.parent)
-  );
-  const moduleRootPath = require("path").resolve(moduleMainFilePath, "../");
-  return moduleRootPath;
+  const { dirname } = require('path')
+  const Module = module.constructor
+  const moduleMainFilePath = dirname(Module._resolveFilename(plugin, module.parent))
+  const moduleRootPath = require('path').resolve(moduleMainFilePath, '../')
+  return moduleRootPath
 }
 
-nexssPlugin.getPluginPath = getPluginPath;
-module.exports = nexssPlugin;
+nexssPlugin.getPluginPath = getPluginPath
+module.exports = nexssPlugin
